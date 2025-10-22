128c53a8101bfcb6f6438f2d89c46de8f1cd01c1
mrbasa <mrbasa@gmail.com>
2025-10-22 13:12:28 -0500

    Fixing output.

--- conf.d/tide_report.fish
+++ conf.d/tide_report.fish
@@ -15,8 +15,8 @@ function _tide_report_install --on-event tide_report_install
     set -q tide_report_weather_language          || set -Ux tide_report_weather_language "en"
     set -q tide_report_weather_unavailable_text  || set -Ux tide_report_weather_unavailable_text ""
     set -q tide_report_weather_unavailable_color || set -Ux tide_report_weather_unavailable_color red
-    set -q tide_weather_color                    || set -Ux tide_weather_color green
-    set -q tide_weather_bg_color                 || set -Ux tide_weather_bg_color red
+    set -q tide_weather_color                    || set -Ux tide_weather_color normal
+    set -q tide_weather_bg_color                 || set -Ux tide_weather_bg_color normal
 
     # --- Moon Module ---
     set -q tide_report_moon_format            || set -Ux tide_report_moon_format "%m"
@@ -41,7 +41,6 @@ function _tide_report_install --on-event tide_report_install
 end
 
 function _tide_report_update --on-event tide_report_update
-    _tide_fish_colorize "Doing a update..."
     _tide_report_install
 end
 
