5f329bf858b7e921178e0ca60bf9c4a5a608630c
Kiel Stock <mrbasa@gmail.com>
2025-10-21 21:23:27 -0500

    Revising appraoch.

--- functions/_tide_report_repaint_handler.fish
+++
@@ -1,6 +0,0 @@
-# TideReport :: Event Handler
-# Listens for update events from the fetchers and triggers a prompt repaint.
-
-function _tide_report_repaint_handler --on-event tide_report_weather_updated --on-event tide_report_moon_updated --on-event tide_report_tide_updated
-    commandline -f repaint
-end
